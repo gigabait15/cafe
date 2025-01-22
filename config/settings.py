0de7473a4d08af1d29869ac94f6b49033d5cb130
gigabait15 <Workkerbecov@yandex.ru>
2025-01-22 17:41:11 +0800

    build serv

--- config/settings.py
+++ config/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-hdlh6cb+2nu2g0(%z117_a$+#blgm$@uqxbwdcvc(^-h@*kvcw
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['lhlb-rllk-835y.gw-1a.dockhost.net', '127.0.0.1']
 
 CORS_ALLOWED_ORIGINS = ['https://lhlb-rllk-835y.gw-1a.dockhost.net']
 
